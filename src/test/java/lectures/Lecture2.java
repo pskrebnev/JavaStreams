02bd418da652fc58ef1f106098d0d5a458eed002
PSkrabneu <skrebnev@gmail.com>
2024-05-22 13:19:38 +0200

    Complete Lecture 2

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -2,6 +2,7 @@ package lectures;
 
 import beans.Person;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 import mockdata.MockData;
 import org.junit.Test;
@@ -10,20 +11,26 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
+    IntPredicate even = x -> x % 3 == 0;
+    IntStream.range(1, 11).filter(even).forEach(System.out::println);
 
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
-
+    IntStream.range(0, people.size())
+        .forEach(index -> {
+          Person person = people.get(index);
+          System.out.println(person);
+        });
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(number -> number % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
   }
 }
