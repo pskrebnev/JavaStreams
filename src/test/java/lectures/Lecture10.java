b051da026fe645fdd2a170190292f50210a30e4e
PSkrabneu <skrebnev@gmail.com>
2025-01-27 13:30:48 +0100

    Some typos fixed

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -3,11 +3,7 @@ package lectures;
 
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
-
-import java.util.Arrays;
 import java.util.Collection;
-
-
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Before;
@@ -41,15 +37,13 @@ public class Lecture10 {
 
     List<String> names = Lists.newArrayList();
 
-    for(List<String> listOfNames : arrayListOfNames) {
-      for(String name : listOfNames) {
+    for (List<String> listOfNames : arrayListOfNames) {
+      for (String name : listOfNames) {
         names.add(name);
       }
     }
 
     System.out.println(names);
-
-
   }
 
   @Test
@@ -64,17 +58,5 @@ public class Lecture10 {
     System.out.println(names);
     System.out.println(System.currentTimeMillis() - start + "ms");
   }
-
-//   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
-
-    List<String> names = arrayListOfNames.stream()
-        .flatMap(List::stream)
-        .collect(Collectors.toList());
-
-    System.out.println(names);
-
-  }
-
-
 }
 
